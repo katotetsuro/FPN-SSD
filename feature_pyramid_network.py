aa2ec007b2b718ff9a2bc2bc5d0e742df301d7be
katotetsuro <nistetsurooy@gmail.com>
2018-01-28 08:20:36 +0900

    adapt fpn to SSD300

--- feature_pyramid_network.py
+++ feature_pyramid_network.py
@@ -14,7 +14,7 @@ _imagenet_mean = np.array((123, 117, 104)).reshape((-1, 1, 1))
 
 class FeaturePyramidNetwork(chainer.Chain):
     insize = 300
-    grids = (38, 19, 10, 4)
+    grids = (38, 19, 10, 5, 3, 1)
 
     def __init__(self):
         super().__init__()
@@ -29,19 +29,23 @@ class FeaturePyramidNetwork(chainer.Chain):
             # conv layer for top-down pathway
             self.conv_p4 = L.Convolution2D(None, 256, ksize=3, stride=1, pad=1)
             self.conv_p3 = L.Convolution2D(None, 256, ksize=3, stride=1, pad=1)
-            #            self.conv_p2 = L.Convolution2D(None, 256, ksize=3, stride=1, pad=1)
+            self.conv_p2 = L.Convolution2D(None, 256, ksize=3, stride=1, pad=1)
+            
+            self.conv_p7 = L.Convolution2D(None, 256, ksize=3, stride=1, pad=1)
 
             # lateral connection
             self.lat_p4 = L.Convolution2D(
                 in_channels=None, out_channels=256, ksize=1, stride=1, pad=0)
             self.lat_p3 = L.Convolution2D(
                 in_channels=None, out_channels=256, ksize=1, stride=1, pad=0)
+            self.lat_p2 = L.Convolution2D(
+                in_channels=None, out_channels=256, ksize=1, stride=1, pad=0)
 
     def __call__(self, x):
         # bottom-up pathway
         h = F.relu(self.resnet.bn1(self.resnet.conv1(x)))
         h = F.max_pooling_2d(h, ksize=(2, 2))
-        c2 = self.resnet.res2(h)
+        c2 = F.max_pooling_2d(self.resnet.res2(h), ksize=(2, 2))
         c3 = self.resnet.res3(c2)
         c4 = self.resnet.res4(c3)
         c5 = self.resnet.res5(c4)
@@ -54,14 +58,21 @@ class FeaturePyramidNetwork(chainer.Chain):
         p3 = self.conv_p3(
             F.unpooling_2d(p4, ksize=2, outsize=(
                 c3.shape[2:4])) + self.lat_p3(c3))
+        
+        p2 = self.conv_p2(
+            F.unpooling_2d(p3, ksize=2, outsize=(
+                c2.shape[2:4])) + self.lat_p2(c2))
 
         # from paper,
         # Here we introduce P6 only for covering a larger anchor scale of 5122.
         # P6 is simply a stride two subsampling of P5
         # but original SSD prepare small( at spatial ) feature maps, so I try stride=3, it produece (4, 4) feature maps.
         p6 = F.max_pooling_2d(p5, ksize=1, stride=3)
+        
+        # adjust to SSD300(experimental)
+        p7 = F.max_pooling_2d(self.conv_p7(p6), ksize=3)
 
-        return p3, p4, p5, p6
+        return p2, p3, p4, p5, p6, p7
 
 
 from chainercv.links.model.ssd import SSD
@@ -77,9 +88,9 @@ class FPNSSD(SSD):
             extractor=FeaturePyramidNetwork(),
             multibox=Multibox(
                 n_class=n_fg_class + 1,
-                aspect_ratios=((2, 3), (2, 3), (2, 3), (2, 3))),
-            steps=(16, 100, 200, 300),
-            sizes=(30, 60, 162, 213, 315),
+                aspect_ratios=((2,), (2, 3), (2, 3), (2, 3), (2,), (2,))),
+            steps=(8, 16, 32, 64, 100, 300),
+            sizes=(30, 60, 111, 162, 213, 264, 315),
             mean=_imagenet_mean)
 
 
